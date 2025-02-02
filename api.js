41e2827d1158dd1d1ead10490b4d00e53c60bbfe
ShyamMardiya8 <shyammardiya8@gmail.com>
2025-02-02 20:35:57 +0530

    REST CRUD api complete

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ const express = require("express")
 const users = require("./MOCK_DATA.json")
 const fs = require("fs")
 const { json } = require("stream/consumers")
-const app = express()
+const app = express() 
 const port = 3000
 app.use(express.json())
 app.use(express.urlencoded({ extended : false}))
