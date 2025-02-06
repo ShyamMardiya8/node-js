0b469097befa646d363cb6f4fbd4dc0332ba172c
ShyamMardiya8 <shyammardiya8@gmail.com>
2025-02-06 21:58:12 +0530

    crud completed using mongodb and express

--- api.js
+++ api.js
@@ -5,7 +5,7 @@ const { json } = require("stream/consumers")
 const mongoose = require("mongoose")
 const { timeStamp } = require("console")
 const app = express() 
-const port = 3000
+const port = 9000
 
 // my mongoose schema
 
